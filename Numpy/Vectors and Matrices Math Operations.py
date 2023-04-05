bda483738d50f0627c0ff8c9ef21184fe0fb6f23
Anxhelo Cenollari <anxhelocenollari@gmail.com>
2023-04-05 17:12:43 +0200

    math operations

---
+++ Numpy/Vectors and Matrices Math Operations.py
@@ -0,0 +1,51 @@
+import numpy as np
+
+
+
+
+# For Math Operations both vectors that take place should have the same dimensions
+
+v1 = np.array([1, 2, 3])
+v2 = np.array([4, 5, 6])
+
+print("v1 vector is: ", v1)
+print("v2 vector is: ", v2)
+
+# Gives us a vector that holds the sum of both vectors element-wise on axis = 0
+print("v1 + v2 operation is: ", v1 + v2)
+print("np.sum function of v1,v2 on axis = 0 is: ", np.sum((v1, v2), axis=0))
+
+# On axis = 1 we get a vector that holds 2 elements: the sum of elements of the v1 and sum of elements of v2
+print("np.sum function of v1,v2 on axis = 1 is: ", np.sum((v1, v2), axis=1))
+
+# Gives us a sum of all elements of the vectors.
+print("np.sum function by default without specifying any axis is: ", np.sum((v1, v2)))
+
+# Gives us the dot product of 2 vectors: 1*4 + 2*5 + 3*6
+v_dot = np.dot(v1, v2)
+print("np.dot function of v1 and v2 is: ", v_dot)
+
+# Gives us a third vector of [ v1[0]*v2[0], v1[1]*v2[1], v1[2]*v2[2] ] -> multiply element-wise on axis=0
+c = v1 * v2
+print("v1 * v2 operations is: ", c)
+print("np.multiply function is: ", np.multiply(v1, v2))
+
+
+
+
+# Addition and subtraction: Matrices must have the same dimensions.
+# Multiplication: For matrix multiplication, the number of columns in the first matrix must match the number of rows in the second matrix.
+
+
+A = np.array([[1, 2],
+              [3, 4],
+              [5, 6]])  # A 3x2 matrix
+
+B = np.array([[7, 8],
+              [9, 10]])  # A 2x2 matrix
+
+C = A @ B  # Matrix multiplication
+print("A * B product is: ", C)
+print("np.matmul function of 2 matrices is: ", np.matmul(A, B))
+print("matrix1.dot(matrix2) is : ", A.dot(B))
+print(" sum of np.matmul function of both vectors is: ", np.matmul(A, B).sum())
--- Prova/prova1.py
+++ Prova/prova1.py
@@ -1,40 +1,36 @@
+'''
+Boston House Price Prediction Project - We used california cuz boston dataset was not anymore available
+In this project you will work on developing an end to end machine learning project using linear regression, as this will be your first
+project in your machine learning journey. We will be doing extensive data visualization, we will perform data feature engineering, we will also see
+how we can select features based on the correlation of the features
+'''
 
 import numpy as np
+import pandas as pd
+
+import plotly.express as px
+import plotly.graph_objects as go
+import plotly.io as pio
+pio.templates
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+# %matplotlib inline
+
+from sklearn.datasets import fetch_california_housing
+fetch_california_housing = fetch_california_housing()
+x = fetch_california_housing.data
+y = fetch_california_housing.target
+
+data = pd.DataFrame(x, columns=fetch_california_housing.feature_names)  #Building a dataframe giving x(indipendent variable), with column name specified
+data["SalePrice"] = y     #saleprice
+# data.head()
+
+print(data.head())
+print(fetch_california_housing.DESCR)
+print(data.shape)
+print(data.info())
+print(data.describe())
 
 
-v1 = np.array([1, 2, 3])
-v2 = np.array([4, 5, 6])
-v_sum = v1 + v2
-print(v_sum)
-print(np.sum((v1,v2),axis=0))
 
-
-v1 = np.array([1, 2, 3])
-v2 = np.array([4, 5, 6])
-v_dot = np.dot(v1, v2)
-print(v_dot)
-
-
-a = np.array([1, 2, 3])
-b = np.array([4, 5, 6])
-c = a * b
-print(c)
-print(np.multiply(a,b))
-print(np.dot(a,b))
-
-
-
-
-A = np.array([[1, 2],
-              [3, 4],
-              [5, 6]])  # A 3x2 matrix
-
-B = np.array([[7, 8],
-              [9, 10]])  # A 2x2 matrix
-
-
-C = A @ B  # Matrix multiplication
-print(C)
-print(np.matmul(A,B))
-print(A.dot(B))
-print(np.matmul(A,B).sum())
