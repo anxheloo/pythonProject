cb7918b2cdb6f032f2cd7c3a4238e70b00256865
Anxhelo Cenollari <anxhelocenollari@gmail.com>
2023-04-06 02:02:01 +0200

    ML exercise

--- Prova/prova1.py
+++ Prova/prova1.py
@@ -1,40 +1,34 @@
+'''Stock Price predictor
+'''
 
 import numpy as np
+import pandas as pd
+
+import yfinance as yf
+
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 
-v1 = np.array([1, 2, 3])
-v2 = np.array([4, 5, 6])
-v_sum = v1 + v2
-print(v_sum)
-print(np.sum((v1,v2),axis=0))
+stocks = input("Enter the code of the stock:- ")
+
+# data = yf.download(stocks, "2022-01-01", "2022-03-03",auto_adjust=True)
+# print(data.head())
+
+msft = yf.Ticker(stocks)
+
+# get all stock info (slow)
+print(msft.info)
+
+# fast access to subset of stock info (opportunistic)
+print(msft.fast_info)
+
+# get historical market data
+hist = msft.history(period="1mo")
+print(hist)
+
+# - accurate time-series count:
+a = msft.get_shares_full(start="2008-01-01", end="2021-01-18")
+print("this is : ", a)
 
 
-v1 = np.array([1, 2, 3])
-v2 = np.array([4, 5, 6])
-v_dot = np.dot(v1, v2)
-print(v_dot)
-
-
-a = np.array([1, 2, 3])
-b = np.array([4, 5, 6])
-c = a * b
-print(c)
-print(np.multiply(a,b))
-print(np.dot(a,b))
-
-
-
-
-A = np.array([[1, 2],
-              [3, 4],
-              [5, 6]])  # A 3x2 matrix
-
-B = np.array([[7, 8],
-              [9, 10]])  # A 2x2 matrix
-
-
-C = A @ B  # Matrix multiplication
-print(C)
-print(np.matmul(A,B))
-print(A.dot(B))
-print(np.matmul(A,B).sum())
