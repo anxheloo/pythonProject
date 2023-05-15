d01c516a180a9344176fb3928d4719b1acdae876
Anxhelo Cenollari <anxhelocenollari@gmail.com>
2023-05-15 16:27:31 +0200

    changes

--- Matplotlib/1-Matplotlib Overview.py
+++ Matplotlib/1-Matplotlib Overview.py
@@ -17,3 +17,8 @@ plt.show()
                                 '-.k' -> dashdot black
                                 ':r' -> dotted red
 '''
+
+fig, ax = plt.subplots()  # Create a figure containing a single axes.
+ax.plot([1, 2, 3, 4], [1, 4, 2, 3])  # Plot some data on the axes.
+
+
